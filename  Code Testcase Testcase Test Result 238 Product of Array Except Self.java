527be4956f7388f8774a2b6c2ad3346cf1d7f93c
gamify-gif <gamify1472004@gmail.com>
2025-05-25 09:19:57 +0000

    first leetcode

---
+++  Code Testcase Testcase Test Result 238 Product of Array Except Self.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int N=nums.length;
+
+        int[] left_product=new int[N];
+        int[] right_product=new int[N];
+
+        left_product[0]=1;
+        right_product[N-1]=1;
+
+        for(int i=1;i<N;i++){
+            left_product[i]=nums[i-1]*left_product[i-1];
+        }
+
+        for(int i=N-2;i>=0;i--){
+            right_product[i]=nums[i+1]*right_product[i+1];
+        }
+
+        int[] answer=new int[N];
+        for(int i=0;i<N;i++){
+            answer[i]=left_product[i]*right_product[i];
+        }
+        
+
+        return answer;
+    }
+}
+
